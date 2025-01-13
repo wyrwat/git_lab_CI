4a831593ce79c4c3dfa06475daf102092ee4fc8e
Tomasz wyrwa <wyrwa.t@gmail.com>
2025-01-13 21:28:01 +0100

    add hedless option

--- shop_sample_test.py
+++ shop_sample_test.py
@@ -1,15 +1,24 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service  # Import Service
 
 class ShopSampleTests(unittest.TestCase):
 
     def setUp(self):
-        print("Setup: Initializing WebDriver")
-        # Use Service to specify the chromedriver path
+        print('Initializing WebDriver')
+
+        options = Options()
+        options.add_argument("--headless")  # Ensure Chrome runs in headless mode
+        options.add_argument("--no-sandbox")  # Disable sandbox for CI environments
+        options.add_argument("--disable-dev-shm-usage")  # Avoid potential issues with shared memory
+        options.add_argument("--remote-debugging-port=9222")  # Optional: for debugging
+
         service = Service(ChromeDriverManager().install())
-        self.driver = webdriver.Chrome(service=service)  # Use service in Chrome()
+
+        self.driver = webdriver.Chrome(service=service, options=options)
+
         self.base_url = "https://autodemo.testoneo.com/en/"
 
     def tearDown(self):
